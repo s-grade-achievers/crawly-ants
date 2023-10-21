db684887ed90222f9d72a9f902b0bc17687562ae
MBUYt0n <shusrith03@gmail.com>
2023-10-21 17:44:55 +0530

    idk

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "func4graph.h"
+
+int main()
+{
+    FILE *file = fopen("idunno.csv", "r");
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        return 1;
+    }
+    antancs *graph = (antancs *)malloc(sizeof(antancs));
+    putdata(graph, file);
+
+    for (int i = 0; i < graph->V; i++)
+        for (int j = 0; j < graph->V; j++)
+        {
+            // if(graph->adjacency_list[i][j] != NULL)
+            printf("%s", graph->adjacency_list[i][j]->dst);
+        }
+    return 0;
+}
