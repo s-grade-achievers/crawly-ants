9c4a77baffa5363a951a6320665362585a8b3b45
MBUYT0N <shusrith03@gmail.com>
2023-10-25 09:50:29 +0530

    WORK HATH BEGUN

--- one.c
+++ one.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Define data structures for the network, ants, and pheromone levels.
 
-struct Node
+typedef struct NODE
 {
-    int id;
-    // Add other attributes (e.g., coordinates, routing table).
-};
+    char src[30];
+    char dst[30];
+    float weight;
+} node;
 
-struct Link
+typedef struct ANTANCS
 {
-    int start_node;
-    int end_node;
-    double pheromone_level;
-};
+    int V;
+    struct NODE *adjacency_list[50][50];
+} antancs;
 
 struct Ant
 {
@@ -25,9 +26,41 @@ struct Ant
 
 // Initialize network, pheromone levels, and ants.
 
-void initializeNetwork(struct Node *nodes, int num_nodes, struct Link *links, int num_links)
+antancs *putdata(antancs *graph, FILE *fp)
 {
-    // Initialize the network topology.
+    if (fp == NULL)
+    {
+        printf("Error opening the file");
+        return graph;
+    }
+
+    char line[300];
+    fgets(line, sizeof(line), fp);
+
+    int i = 0, j = 0;
+    char *token = malloc(30 * sizeof(char)); // Allocate memory for token
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        if (line[0] != '\n')
+        {
+            char *src = (char *)malloc(30 * sizeof(char));  // Allocate memory for src
+            char *dest = (char *)malloc(30 * sizeof(char)); // Allocate memory for src
+            src = strtok(line, ",");
+            dest = strtok(NULL, ",");
+            float w = atof(strtok(NULL, ","));
+            graph->adjacency_list[i][j] = (node *)malloc(sizeof(node));
+            strcpy(graph->adjacency_list[i][j]->src, src);
+            strcpy(graph->adjacency_list[i][j]->dst, dest);
+            graph->adjacency_list[i][j]->weight = w;
+            j++;
+        }
+        else
+        {
+            i++;
+            j = 0;
+        }
+    }
+    return graph;
 }
 
 void initializePheromoneLevels(struct Link *links, int num_links)
@@ -57,11 +90,15 @@ void acoAlgorithm(struct Node *nodes, int num_nodes, struct Link *links, int num
 
 int main()
 {
-    // Define and initialize the network, ants, and pheromone levels.
-
-    // Run the ACO algorithm.
-
-    // Retrieve the best route and output the result.
+    FILE *file = fopen("new.csv", "r");
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        return 1;
+    }
+    antancs *graph = (antancs *)malloc(sizeof(antancs));
+    graph->V = 50;
+    putdata(graph, file);
 
     return 0;
 }
