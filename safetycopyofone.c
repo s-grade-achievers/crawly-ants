f12ce3ac224ba0e01bc42fc9ac11d194b2361c2c
MBUYT0N <shusrith03@gmail.com>
2023-10-26 10:08:59 +0530

    slight changes

---
+++ safetycopyofone.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define data structures for the network, ants, and pheromone levels.
+
+typedef struct NODE
+{
+    char src[30];
+    char dst[30];
+    float weight;
+} node;
+
+typedef struct ANTANCS
+{
+    int V;
+    struct NODE *adjacency_list[50][50];
+} antancs;
+
+struct Ant
+{
+    int current_node;
+    int destination_node;
+    // Add path history and other attributes.
+};
+
+// Initialize network, pheromone levels, and ants.
+
+antancs *putdata(antancs *graph, FILE *fp)
+{
+    if (fp == NULL)
+    {
+        printf("Error opening the file");
+        return graph;
+    }
+
+    char line[300];
+    fgets(line, sizeof(line), fp);
+
+    int i = 0, j = 0;
+    char *token = malloc(30 * sizeof(char)); // Allocate memory for token
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        if (line[0] != '\n')
+        {
+            char *src = (char *)malloc(30 * sizeof(char));  // Allocate memory for src
+            char *dest = (char *)malloc(30 * sizeof(char)); // Allocate memory for src
+            src = strtok(line, ",");
+            dest = strtok(NULL, ",");
+            float w = atof(strtok(NULL, ","));
+            graph->adjacency_list[i][j] = (node *)malloc(sizeof(node));
+            strcpy(graph->adjacency_list[i][j]->src, src);
+            strcpy(graph->adjacency_list[i][j]->dst, dest);
+            graph->adjacency_list[i][j]->weight = w;
+            j++;
+        }
+        else
+        {
+            i++;
+            j = 0;
+        }
+    }
+    return graph;
+}
+
+void initializePheromoneLevels(struct Link *links, int num_links)
+{
+    // Initialize pheromone levels on links.
+}
+
+void initializeAnts(struct Ant *ants, int num_ants, int source_node, int destination_node)
+{
+    // Initialize ants, set source and destination nodes, and other attributes.
+}
+
+// Main ACO algorithm.
+
+void acoAlgorithm(struct Node *nodes, int num_nodes, struct Link *links, int num_links, struct Ant *ants, int num_ants)
+{
+    int max_iterations = 100;
+    double evaporation_rate = 0.1;
+    double alpha = 1.0; // Pheromone influence.
+    double beta = 2.0;  // Heuristic influence.
+
+    for (int iteration = 1; iteration <= max_iterations; iteration++)
+    {
+        
+    }
+}
+
+int main()
+{
+    FILE *file = fopen("new.csv", "r");
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        return 1;
+    }
+    antancs *graph = (antancs *)malloc(sizeof(antancs));
+    graph->V = 50;
+    putdata(graph, file);
+
+    return 0;
+}
