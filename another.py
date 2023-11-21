c3ba25801059edc4481498a5babc09aa9ba05e79
MBUYT0N <shusrith03@gmail.com>
2023-11-21 12:02:18 +0530

    cities.txt

--- another.py
+++ another.py
@@ -29,3 +29,17 @@
 
 # s = "0 5 2 9 8 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42"
 # print(len(s.split(" ")))
+f = open("newer.csv", "r")
+f = f.readlines()
+f = [i.split(",") for i in f]
+l = []
+for i in f:
+    l.append(i[0])
+l = list(set(l))
+l = sorted(l)
+l = l[1:]
+l = [i[2:] for i in l]
+f1 = open("cities.txt", "w")
+for i in l:
+    f1.write(i)
+    f1.write("\n")
